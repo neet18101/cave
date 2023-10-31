f516ad182ed2c4a29044167cea15d4533bce4800
Navneet <navneetyadav9198977444@gmail.com>
2023-10-31 23:44:07 +0530

    newUpdate

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Testing />} />
-        <Route path='/home' element={<Home />} />
+        <Route path='/' element={<Home />} />
+        {/* <Route path='/home' element={<Home />} /> */}
       </Routes>
     </BrowserRouter>
   )
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from '../Components/Layout/Header/Header'
+import Testing from './Testing'
+import Featured from '../Components/Layout/Featured/Featured'
 
 const Home = () => {
     return (
         <>  
 
-            <Header />
+            <Testing />
+            <Featured/>
 
 
         </>
