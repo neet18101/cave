9594f230237c96c6fa0a4120fc5106dd6c5d466d
Navneet <navneetyadav9198977444@gmail.com>
2023-11-03 08:54:48 +0530

    signupdesgin

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,42 +1,54 @@
-import React from 'react'
-import './signup.css'
+import React from "react";
+import "./signup.css";
 const Signup = () => {
-    return (
-        <>
-            <div className='container-fluid'>
-                <div className="row">
-                    <div className="col-lg-6 signupLeft">
-                        <div className="text-left">
-                            <a className="navbar-brand " href="#">
-                                <img
-                                    decoding="async"
-                                    src="/icons/brand1.svg"
-                                    className="img-fluid navbar-brand-sticky"
-                                />
-                                BachelorsCave
-                            </a>
-                        </div>
-
-                        <div className='text-center signupParentDiv'>
-                            <span className='signupText'>A House is made of bricks
-                                and beams.A Home is made
-                                of hope and dreams.
-                                We’ll help you find your home.</span>
-                        </div>
-
-                        <div className='d-flex flex-column align-items-end'>
-                            <img className='signupImg img-fluid' src="/icons/signup.png" alt="" />
-                        </div>
-                    </div>
-                    <div className="col-lg-6"></div>
-                </div>
-
-
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-lg-6 signupLeft">
+            <div className="text-left">
+              <a className="navbar-brand " href="#">
+                <img
+                  decoding="async"
+                  src="/icons/brand1.svg"
+                  className="img-fluid navbar-brand-sticky"
+                />
+                BachelorsCave
+              </a>
             </div>
 
+            <div className="text-center signupParentDiv">
+              <span className="signupText">
+                A House is made of bricks and beams.A Home is made of hope and
+                dreams. We’ll help you find your home.
+              </span>
+            </div>
 
-        </>
-    )
-}
+            <div className="d-flex flex-column align-items-end">
+              <img
+                className="signupImg img-fluid"
+                src="/icons/signup.png"
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <div className="createAccountDiv"></div>
+            <h2 className="text-center">Create Account</h2>
 
-export default Signup
+            <div className="button-box">
+              <button type="button" className="toggle-btn">
+                owner signup
+              </button>{" "}
+              <button type="button" className="toggle-btn">
+                Tenant signup
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Signup;
