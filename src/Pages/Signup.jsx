d11fab422ab0b17d285b235ae71c1af8940fbb32
Navneet <navneetyadav9198977444@gmail.com>
2023-11-04 10:20:46 +0530

    formupdate

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -34,15 +34,49 @@ const Signup = () => {
           </div>
           <div className="col-lg-6">
             <div className="createAccountDiv"></div>
-            <h2 className="text-center">Create Account</h2>
+            <h2
+              className="text-center"
+              style={{
+                fontSize: "40px",
+                fontWeight: "800",
+                letterSpacing: "10px",
+              }}
+            >
+              Create Account
+            </h2>
 
             <div className="button-box">
+              <div id="btn"></div>
               <button type="button" className="toggle-btn">
                 owner signup
               </button>{" "}
               <button type="button" className="toggle-btn">
                 Tenant signup
               </button>
+            </div>
+            <div className="social-icons mt-3">
+              <div className="row">
+                <div className="googleButton">
+                  <button className="lognwithbutton">
+                    <img
+                      src="/icons/devicon_google.svg"
+                      alt=""
+                      style={{ marginRight: "20px", width: "15px" }}
+                    />
+                    <span className="loginWithgoogle">Login with Google</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="account__login--divide">
+              <span className="account__login--divide__text">OR</span>
+            </div>
+            <div className="text-center">
+              <form action="">
+                <div className="form-group">
+                  <input type="text" />
+                </div>
+              </form>
             </div>
           </div>
         </div>
