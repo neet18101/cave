672dc3cc287574516207aa9bccfe257571c84bbd
Navneet <navneetyadav9198977444@gmail.com>
2024-01-04 23:13:21 +0530

    hello

--- src/Components/Layout/Header/SubNavbar.jsx
+++ src/Components/Layout/Header/SubNavbar.jsx
@@ -86,6 +86,7 @@ const SubNavbar = () => {
             >
               <span className="ti-align-justify navbar-toggler-icon" />
             </button>
+            {}
             {sortedData && sortedData != null ? (
               <>
                 <div className="nav__right d-flex justify-content-between align-items-center">
